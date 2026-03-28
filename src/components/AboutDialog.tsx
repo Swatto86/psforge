3d8da01534f859b7a5ff6cb97fbc0548783fc6dd
Swatto <58915441+Swatto86@users.noreply.github.com>
2026-03-28 10:51:34 +0000

    feat: update version to 1.0.6 and modify related documentation and scripts

--- src/components/AboutDialog.tsx
+++ src/components/AboutDialog.tsx
@@ -22,7 +22,7 @@ export function AboutDialog() {
   useEffect(() => {
     getVersion()
       .then((v) => setVersion(v))
-      .catch(() => setVersion("1.0.5"));
+      .catch(() => setVersion("1.0.6"));
   }, []);
 
   // Close on Escape key.
