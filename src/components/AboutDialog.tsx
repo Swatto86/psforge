6d253f10107a62f80319afee9bc8c431da2277ae
Swatto <58915441+Swatto86@users.noreply.github.com>
2026-04-05 13:12:43 +0100

    chore: bump version to 1.2.0

--- src/components/AboutDialog.tsx
+++ src/components/AboutDialog.tsx
@@ -22,7 +22,7 @@ export function AboutDialog() {
   useEffect(() => {
     getVersion()
       .then((v) => setVersion(v))
-      .catch(() => setVersion("1.1.0"));
+      .catch(() => setVersion("1.2.0"));
   }, []);
 
   // Close on Escape key.
